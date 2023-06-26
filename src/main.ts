219b94e56a14c62a8e69a7ed06cb95a1c058c362
Ibrahim Mahamat zene <mahamat-zene.ibrahim@mail.de>
2023-06-26 15:37:52 +0200

    pages workt

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
-import './style.scss'
+import './style.scss';
+import { getMenu, getCurrentPage } from './Router';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML =/*html*/`
-<h1>Hello</h1>
-
-`
+document.querySelector<HTMLDivElement>('#app')!.innerHTML =/* html */`
+<header>
+	<h1>menu-site</h1>
+	${getMenu()}
+</header>
+<main>
+	${getCurrentPage()}
+</main>
+`;
